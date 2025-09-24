d2a9537ff47f5b07e4ef13cb7b75c5a993b4363b
mahdiehgh79 <mahdiehghasemi79@yahoo.com>
2025-09-25 00:11:55 +0330

    add some styles with tailwind classname

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">fast react pizza</Link>
       <SearchOrder />
-      <Link to="/">fast react pizza</Link>
+
+      <Username/>
+      
     </header>
   );
 }
