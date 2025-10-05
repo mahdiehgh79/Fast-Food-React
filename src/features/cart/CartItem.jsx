381b4ee8b99426eb885680d4a614bd787e4b8d91
mahdiehgh79 <mahdiehghasemi79@yahoo.com>
2025-10-05 23:22:31 +0330

    add some styles and create ButtonComponent

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,10 +1,13 @@
+import PropTypes from 'prop-types';
+import { formatCurrency } from '../../../../../ultimate-react-course-main/ultimate-react-course-main/16-fast-react-pizza/final-1-after-tailwind/src/utils/helpers';
+
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+  const { name, quantity, totalPrice } = item;
 
   return (
     <li>
       <p>
-        {quantity}&times; {name}
+        {quantity} &times; {name}
       </p>
       <div>
         <p>{formatCurrency(totalPrice)}</p>
@@ -12,5 +15,14 @@ function CartItem({ item }) {
     </li>
   );
 }
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    pizzaId: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    totalPrice: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default CartItem;
